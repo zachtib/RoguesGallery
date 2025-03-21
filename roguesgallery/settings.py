296903e4d53991f982a7c2edb5395336a3f71019
Zach Tibbitts <zachtib@gmail.com>
2025-03-21 13:33:34 -0400

    async/redis backed channels

--- roguesgallery/settings.py
+++ roguesgallery/settings.py
@@ -74,7 +74,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'roguesgallery.wsgi.application'
+
 ASGI_APPLICATION = 'roguesgallery.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
